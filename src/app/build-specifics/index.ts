d034106976752dbc3d639f049fe7714ed6a35a20
Knut Schleßelmann <knut.schlesselmann@fortis-it.de>
2024-02-19 14:28:09 +0100

    build: update ngrx to official v17 release

--- src/app/build-specifics/index.ts
+++ src/app/build-specifics/index.ts
@@ -1,5 +1,5 @@
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 export const extModules = [
-  StoreDevtoolsModule.instrument({ maxAge: 25 }),
+  StoreDevtoolsModule.instrument({ maxAge: 25 , connectInZone: true}),
 ];
